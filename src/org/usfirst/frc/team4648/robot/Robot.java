aba76639c0b21516473187a2b9e2b96743c6b5b0
Manuel Quinones <manuel.quinones@target.com>
2018-01-20 12:54:12 -0600

    Adding timeouts.

--- src/org/usfirst/frc/team4648/robot/Robot.java
+++ src/org/usfirst/frc/team4648/robot/Robot.java
@@ -108,7 +108,6 @@ public class Robot extends TimedRobot {
 		if (m_autonomousCommand != null) {
 			m_autonomousCommand.cancel();
 		}
-		driveCommand.start();
 	}
 
 	/**
--- src/org/usfirst/frc/team4648/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team4648/robot/subsystems/DriveSubsystem.java
@@ -10,7 +10,7 @@ package org.usfirst.frc.team4648.robot.subsystems;
 import org.usfirst.frc.team4648.robot.RobotMap;
 import org.usfirst.frc.team4648.robot.commands.DriveCommand;
 
-import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
@@ -20,10 +20,17 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class DriveSubsystem extends Subsystem { 
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
-//	private final SpeedController speedController1 = RobotMap.leftDriveMotorController;
-//    private final SpeedController speedController2 = RobotMap.rightDriveMotorController;
+	private final Talon speedController1 = RobotMap.leftDriveMotorController;
+    private final Talon  speedController2 = RobotMap.rightDriveMotorController;
     private final DifferentialDrive differentialDrive1 = RobotMap.drivetrain;
 
+    public DriveSubsystem() {
+    	addChild("Left CIM", (Talon) speedController1);
+	addChild("Right CIM", (Talon) speedController2);
+	differentialDrive1.setSafetyEnabled(true);
+	differentialDrive1.setExpiration(0.1);
+	differentialDrive1.setMaxOutput(1.0);
+    }
 	
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
