d40dd365833187c7fd1e2ad72102f3bb552f9b6a
Unknown <mslaubaugh@isd717.org>
2018-02-15 20:29:47 -0600

    Commenting out lift command

--- src/org/usfirst/frc/team4648/robot/commands/LiftCommand.java
+++ src/org/usfirst/frc/team4648/robot/commands/LiftCommand.java
@@ -29,7 +29,7 @@ public class LiftCommand extends Command {
 	
 	@Override
 	protected void end() {
-		Robot.liftSubsystem.liftSystemControl(0);
+		//Robot.liftSubsystem.liftSystemControl(0);
 		// TODO Auto-generated method stub
 		super.end();
 	}
